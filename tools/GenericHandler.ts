5536d26e6e92fb962199f39df60f20a7c784e05d
Pter <longpp17@163.com>
2024-06-13 22:09:14 +0200

    added test for generic procedure and modified some

---
+++ tools/GenericHandler.ts
@@ -0,0 +1,58 @@
+import { Number, type Static } from 'runtypes';
+
+const Integer = Number.withConstraint(n => n % 1 === 0, { name: 'Integer' });
+
+type Int = Static<typeof Integer>;
+
+
+export class generic_procedure{
+    default_handler: (args: any[]) => any
+    metaData: generic_procedure_metadata
+    constructor(arity: Int, default_handler: (args: any[]) => any){
+        this.metaData = make_generic_metadata(arity, default_handler)
+        this.default_handler = default_handler
+    }
+    private dispatch(args: any[]){
+        const matched_metadata = this.metaData.metaData.filter(rule => rule.predicate(args))
+        if(matched_metadata.length === 0){
+            throw new Error("Generic handler failed")
+    }
+    return matched_metadata[0].handler(args)
+    }
+
+    public define_handler(predicate: (args: any[]) => boolean, handler: (args: any[]) => any){
+        this.metaData.metaData.unshift({predicate, handler})
+    }
+
+    public execute(args: any[]){
+        return this.dispatch(args)
+    }
+}
+
+
+
+
+
+function make_generic_metadata( arity: Int, handler: (args: any[]) => any): generic_procedure_metadata{
+    const metaData : generic_procedure_metadata = {
+        arity: arity,
+        metaData: [{predicate: () => true, handler: handler}],
+        default_handler: handler
+    }
+    return metaData
+}
+
+
+export interface generic_procedure_metadata{
+    arity: Int
+    metaData: {predicate: (args: string[]) => boolean, handler: (args: string[]) => any}[]
+    default_handler: (args: any[]) => any
+}
+
+
+
+export interface generic_procedure_handler{
+    generic_procedure: generic_procedure
+    applicability: (args: string[]) => boolean
+    handler: (args: any[]) => any
+}
