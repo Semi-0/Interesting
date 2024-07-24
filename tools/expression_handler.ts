6fec60fb3760bd03069482c4c77937968fde082b
Pter <longpp17@163.com>
2024-07-24 17:39:15 +0200

    adapted

--- tools/expression_handler.ts
+++ tools/expression_handler.ts
@@ -6,20 +6,17 @@ import { isSucceed } from "pmatcher/Predicates";
 import { MatchResult } from "pmatcher/MatchResult/MatchResult";
 import { construct_advice, install_advice } from "./Advice";
 
+export const no_change : (proc:any) => any =  (proc: any) => { return proc }
+
+
 export function matcher_advice() : any[]{
     var matchResult: MatchResult | null = null 
 
-    const input_modifers =  [(proc: any) =>{ return proc },
+    const input_modifers =  [no_change,
                              (expr: any[]) => { 
                                 return (input: any[], ...args: any[]) => {
-                               
                                     matchResult = match(input, expr)
-                                    if (isSucceed(matchResult)){
-                                        return true
-                                    }
-                                    else{
-                                        return false
-                                    }
+                                    return isSucceed(matchResult)
                                 }
                               },
                               (handler: (mresult: MatchResult, ...args: any[]) => any) => { 
@@ -27,20 +24,20 @@ export function matcher_advice() : any[]{
                                     // @ts-ignore
                                     return handler(matchResult, ...args)
                                 }}]
-    return construct_advice(input_modifers, (result: any) => { return result})
+    return construct_advice(input_modifers, no_change)
 }
 
 export function log_advice(){
     var input: any[] = []
     return construct_advice([ 
-        (proc: any) => {  return proc},
+        no_change,
         (expr: any[], ...args: any[]) => { input = expr; return expr},
         (handler: any) => {return (mresult: MatchResult, ...args:any[]) => {
             console.log("input", input)
             console.log("match result", mresult)
             return handler(mresult, ...args)
         }}], 
-        (a: any) => {  return a})
+        no_change)
 }
 
 export const define_generic_matcher = install_advice(matcher_advice(), define_generic_procedure_handler)
